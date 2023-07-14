08f9d033cecac506d91af1a25b9f883462b4ec60
drisyaathul <drisyak809@gmail.com>
2023-07-14 18:35:24 +0800

    3.Strings are Equal or Not

---
+++ src/com/bridgelabz/TwoStringsAreEqualOrNot.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class TwoStringsAreEqualOrNot {
+    public static void main(String[] args) {
+        String string1 = "Apple";
+        String string2 = "Orange";
+
+        if (string1 == string2) {
+            System.out.println("Two Strings are Equal");
+        } else {
+            System.out.println("Two Strings are NOT Equal");
+        }
+
+    }
+}
