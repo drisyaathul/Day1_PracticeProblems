3dbe5e0e3f860392bd90571405bc9f02119ac3d8
drisyaathul <drisyak809@gmail.com>
2023-07-14 18:23:16 +0800

    Added: 2.Primitive DataType

---
+++ src/com/bridgelabz/PrimitiveDataType.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+public class PrimitiveDataType {
+
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    char g ;
+    boolean h;
+
+    public static void main(String[] args) {
+
+        PrimitiveDataType dataType = new PrimitiveDataType ();
+        System.out.println("The Default value of byte is " +dataType.a);
+        System.out.println("The Default value of short is " +dataType.b);
+        System.out.println("The Default value of int is " +dataType.c);
+        System.out.println("The Default value of long is " +dataType.d);
+        System.out.println("The Default value of float is " +dataType.e);
+        System.out.println("The Default value of double is " +dataType.f);
+        System.out.println("The Default value of char is " +dataType.g);
+        System.out.println("The Default value of boolean is " +dataType.h);
+
+    }
+}
