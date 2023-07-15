6a42f2960bb81d97b535153e76e660586a621d02
drisyaathul <drisyak809@gmail.com>
2023-07-16 00:07:33 +0800

    5.Name Using Command Line Arguments

--- src/com/bridgelabz/NameUsingCommandLine.java
+++ src/com/bridgelabz/NameUsingCommandLine.java
@@ -3,9 +3,7 @@ package com.bridgelabz;
 //To get the name using the command line
 public class NameUsingCommandLine {
     public static void main(String[] args) {
-        System.out.println("Name Using Command Line Arguments:-");
-        for (String string: args) {
-            System.out.println(string);
+        System.out.println(args[0]);
         }
     }
-}
+
