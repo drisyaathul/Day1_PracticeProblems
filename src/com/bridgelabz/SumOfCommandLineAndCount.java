d8d722227f02404fbcb17120fea64b91a20bea49
drisyaathul <drisyak809@gmail.com>
2023-07-15 23:55:53 +0800

    4.Sum of command-line arguments and count the invalid integers entered

--- src/com/bridgelabz/SumOfCommandLineAndCount.java
+++ src/com/bridgelabz/SumOfCommandLineAndCount.java
@@ -3,10 +3,17 @@ package com.bridgelabz;
 
 public class SumOfCommandLineAndCount {
     public static void main(String[] args) {
-        int sum=0;
-        for(int i=1; i<args.length; i++){
-            sum=sum+Integer.parseInt(args[i]);
+        int sum = 0;
+        int invalid = 0;
+        for(int i=0; i<args.length; i++) {
+            try {
+                sum = sum + Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                invalid++;
+            }
         }
-        System.out.println(sum);
+        System.out.println("Number of Arguments : "+args.length);
+        System.out.println("Sum : "+sum);
+        System.out.println("Invalid Integers : "+invalid);
     }
 }
